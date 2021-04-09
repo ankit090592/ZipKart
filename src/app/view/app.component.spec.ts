417ef80f4d7b481da9a7ba16eedeba55da83c20a
Appinventiv(AAppinventivas-397S-397) <ankit.mehrotra@appinventiv.com>
2021-04-09 11:23:38 +0530

    initialCommit

--- src/app/view/app.component.spec.ts
+++ src/app/view/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'e-comm'`, () => {
+  it(`should have as title 'ZipKart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('e-comm');
+    expect(app.title).toEqual('ZipKart');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('e-comm app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ZipKart app is running!');
   });
 });
